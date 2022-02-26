79d698e062eb3c71a1245f518e3092f2ae69c8c4
WuHao <2323333339@qq.com>
2022-02-26 12:46:43 +0800

    style: change code style

--- generator/index.js
+++ generator/index.js
@@ -3,30 +3,20 @@ module.exports = (api, { version, language, styleSort }) => {
     scripts: {
       vbslint: "eslint . --ext .js,.ts,.vue --fix",
     },
+    gitHooks: {
+      "pre-commit": "lint-staged",
+      "commit-msg": "node ./cli/commit.js",
+    },
     devDependencies: {
       eslint: "^8.9.0", // add eslint
-      prettier: "^2.5.1", // add prettier
       yorkie: "2.0.0", // add git hooks
+      prettier: "^2.5.1", // add prettier
       "lint-staged": "^12.3.4", // add lint-staged to lint git add .
       "vue-eslint-parser": "^8.3.0", // add vue-eslint-parser
       "eslint-plugin-vue": "^8.5.0", // add eslint-plugin-vue
       "eslint-plugin-prettier": "^4.0.0", // add eslint-plugin-prettier
       "eslint-config-prettier": "^8.4.0", // add eslint-config-prettier, to resolve the conflict between prettier and eslint
     },
-    gitHooks: {
-      "pre-commit": "lint-staged",
-      "commit-msg": "node ./cli/commit.js",
-    },
-  };
-
-  const eslintrc = {
-    parser: language === "typescript" ? "@typescript-eslint/parser" : "@babel/eslint-parser",
-    extends: [
-      version === "2.x" ? "plugin:vue/recommended" : "plugin:vue/vue3-recommended",
-      "eslint:recommended",
-      ...(language === "typescript" ? ["plugin:@typescript-eslint/recommended"] : []),
-      "plugin:prettier/recommended",
-    ],
   };
 
   // add style format
@@ -38,6 +28,16 @@ module.exports = (api, { version, language, styleSort }) => {
     package.devDependencies["@babel/core"] = "^7.17.5";
     package.devDependencies["@babel/eslint-parser"] = "^7.17.0";
   }
+
+  const eslintrc = {
+    parser: language === "typescript" ? "@typescript-eslint/parser" : "@babel/eslint-parser",
+    extends: [
+      version === "2.x" ? "plugin:vue/recommended" : "plugin:vue/vue3-recommended",
+      "eslint:recommended",
+      ...(language === "typescript" ? ["plugin:@typescript-eslint/recommended"] : []),
+      "plugin:prettier/recommended",
+    ],
+  };
 
   // render template folder
   api.render("./template", { eslintrc });
